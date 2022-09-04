721371ee3490a87209079cac248d1d5a7282c407
Burko-Oleksandra <109693857+Burko-Oleksandra@users.noreply.github.com>
2022-09-04 22:34:43 +0000

    Deploying to gh-pages from @ Burko-Oleksandra/FoodSta@942849814c7730ff7f078c12caf4d02b1b520cb3 🚀

---
+++ index.81a6df7b.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".modal-open"),closeModalBtn:document.querySelector(".close-modal"),modal:document.querySelector(".modal-js")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.81a6df7b.js.map
---
+++ index.d8ab6920.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".open-menu"),closeMenuBtn:document.querySelector(".close-menu"),menu:document.querySelector(".mob-menu")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.d8ab6920.js.map
