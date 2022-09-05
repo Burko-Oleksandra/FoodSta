299f415a81c5491c49e5d4d89120d34c73ce111f
Mike <hodosovsky@gmail.com>
2022-09-05 13:23:16 +0300

    back to up add rule

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -2,7 +2,7 @@ function backToTop() {
   let button = $('.back-to-top');
 
   $(window).on('scroll', () => {
-    if ($(this).scrollTop() >= 72) {
+    if ($(this).scrollTop() >= 72 && $(this).scrollTop() <= 1000) {
       button.fadeIn();
     } else {
       button.fadeOut();
