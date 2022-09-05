1d5dc355c2ab0aff96dca99d7fbd5bdad1ea8e15
Burko-Oleksandra <109693857+Burko-Oleksandra@users.noreply.github.com>
2022-09-05 13:31:37 +0000

    Deploying to gh-pages from @ Burko-Oleksandra/FoodSta@d6c15252b96e64e887c7dcb15ba1ac27a7e4520b 🚀

--- index.81a6df7b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector(".modal-open"),closeModalBtn:document.querySelector(".close-modal"),modal:document.querySelector(".modal-js")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.81a6df7b.js.map
