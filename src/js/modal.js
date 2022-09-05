d6c15252b96e64e887c7dcb15ba1ac27a7e4520b
Oleksandra <sashkaburko1994@gmail.com>
2022-09-05 16:30:54 +0300

    fixed modal problem

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,12 +3,22 @@
     openModalBtn: document.querySelector('.modal-open'),
     closeModalBtn: document.querySelector('.close-modal'),
     modal: document.querySelector('.modal-js'),
+    body: document.querySelector('body'),
   };
+  document.body.addEventListener('click', toggleModal);
+  // refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function toggleModal(event) {
+    console.log(event.currentTarget);
+    console.log(event.target);
+    if (
+      event.target.classList.contains('modal-open') ||
+      event.target.classList.contains('close-modal')
+    ) {
+      console.log('2');
+      refs.modal.classList.toggle('is-hidden');
+      refs.body.classList.toggle('no-scroll');
+    }
   }
 })();
