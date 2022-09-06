b3bf0205cda21d8d3bb6634db3fce3bef795b84d
Oleksandra <sashkaburko1994@gmail.com>
2022-09-06 10:25:01 +0300

    fixed close-button

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('.modal-open'),
-    closeModalBtn: document.querySelector('.close-modal'),
+    openModalBtn: document.querySelectorAll('.modal-open'),
+    closeModalBtn: document.querySelectorAll('.close-modal'),
     modal: document.querySelector('.modal-js'),
     body: document.querySelector('body'),
   };
-  document.body.addEventListener('click', toggleModal);
-  // refs.openModalBtn.addEventListener('click', toggleModal);
-  // refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal(event) {
-    console.log(event.currentTarget);
-    console.log(event.target);
-    if (
-      event.target.classList.contains('modal-open') ||
-      event.target.classList.contains('close-modal')
-    ) {
-      console.log('2');
-      refs.modal.classList.toggle('is-hidden');
-      refs.body.classList.toggle('no-scroll');
-    }
+  refs.openModalBtn.forEach(item => {
+    item.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.forEach(item => {
+    item.addEventListener('click', toggleModal);
+  });
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
